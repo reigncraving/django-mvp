581d6f652b9b036be9d043b17f5674a9ebca08b8
reigncraving <reigncraving@yahoo.com>
2025-09-16 15:13:08 +0300

    Added Dockerfile and build script

---
+++ api/scripts/build-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+
+local_build() {
+  docker build \
+    --build-arg PYTHON_VER=3.13.7 \
+    --build-arg DEBIAN_FLAVOR=trixie \
+    -t alpha-apartments:local-latest \
+    docker/local/django/
+}
+
+echo "🐋 Building docker images..."
+while test $# -gt 0; do
+  case "$1" in
+  -l | --local)
+    local_build
+    echo "local.."
+    return 1
+    shift
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
