90ba03278025a22083ab15c2e3dd1c76aad55927
reigncraving <reigncraving@yahoo.com>
2025-09-22 08:00:03 +0300

    Refactored docker compose setup

--- api/docker/local/django/start.sh
+++ api/docker/local/django/start.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-python manage.py migrate --no-input
-python manage.py colecstatic --no-input
-exec python manage.py runserver 0.0.0.0:8000
+python manage.py migrate --no-input --settings config.settings.${BUILD_ENVIRONMENT}
+python manage.py collectstatic --no-input --settings config.settings.${BUILD_ENVIRONMENT}
+python manage.py runserver 0.0.0.0:8000 --settings config.settings.${BUILD_ENVIRONMENT}
